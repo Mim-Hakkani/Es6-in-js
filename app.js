9e68470a447a9d35393770e1dfdf361577ce0727
Mim-Hakkani <mimhakkani4g@gmail.com>
2023-02-12 16:11:12 +0600

    use of var and used of multiline arrow function

--- app.js
+++ app.js
@@ -1,4 +1,26 @@
 let name = "Ehsan marketing"
 const name2 = "Employee of Ehsan Marketing "
 
-console.log(name +"\n"+ name2)
+// console.log(name +"\n"+ name2)  
+
+
+/* Arrow Function  */
+
+const number = num =>2*num;   // if it has one parameter 
+// console.log(number(10))
+
+
+const summation =(a,b,c)=>a+b+c;
+// console.log(summation(1,2,5))
+
+const ByePhone  = taka =>{
+    var brand ; 
+    if(taka<5000) {
+        brand = "Button"
+    }
+    else { brand = "Smart"}
+
+    return `you have bye a ${brand} phone`;
+}
+
+console.log(ByePhone(2658));
